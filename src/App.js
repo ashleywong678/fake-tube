8c4fae53c783138c02ba1238e0115c7b8f370c81
Ashley <ashleywong678@gmail.com>
2019-10-11 10:44:53 -0400

    changed component to a class component, imported youtube library, created state for video list and current video, started a handleSubmit function, SearchBar component will take handle submit as a prop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import YouTube from 'react-youtube';
 import './App.css';
 import SearchBar from './components/SearchBar'
+import youtube from '../apis/youtube'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <SearchBar />
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    videos: [],
+    currentVideo: null
+  }
+
+  handleSubmit = async ()
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SearchBar handleSearchSubmit=-{this.handleSubmit}/> {/* sends handleSubmit function to searchbar component */}
+          
+        </header>
+      </div>
+    );
+    }
 }
 
 export default App;
