e25cab358ba3fdb0787894b0970ae6d57d8153f2
Ashley <ashleywong678@gmail.com>
2019-10-11 21:09:49 -0400

    init commit, built basic functional component

---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoListCard from './VideoListCard'
+
+const VideoList = () => {
+
+  return(
+    <div>My list of videos</div>
+  )
+
+}
+
+export default VideoList
