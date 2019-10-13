92da85d1ac308b1463df6b13dfffead7eb2a1c0f
Ashley <ashleywong678@gmail.com>
2019-10-13 10:00:12 -0400

    deconstructed video prop, returned iframe of video and description of video

--- src/components/CurrentVideo.js
+++ src/components/CurrentVideo.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const CurrentVideo = () => {
+const CurrentVideo = ({video}) => {
+
+  const url = `https://www.youtube.com/embed/${video.id.videoId}`
 
   return(
-    <span className='current'>
-      My current Video
-    </span>
+    <div className='current'>
+      <iframe className='embed-responsive-item' src={url} />
+      <p className='description'>{video.snippet.description}</p>
+    </div>
   )
 
 }
