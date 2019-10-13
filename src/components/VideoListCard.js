5ba736f71782afe2f6c564a00a5788169a7fff6b
Ashley <ashleywong678@gmail.com>
2019-10-12 21:51:39 -0400

    displayed incoming video, playing around with div and span for styling

--- src/components/VideoListCard.js
+++ src/components/VideoListCard.js
@@ -3,12 +3,12 @@ import React from 'react'
 const VideoListCard = ({video, handleVideoSelect}) => {
 console.log(video)
   return(
-    <span onClick={() => handleVideoSelect(video)}>
-      <img className='list-item' src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
-      <div>
+    <div className='list-card' onClick={() => handleVideoSelect(video)}>
+      <img className='list-pic' src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
+      <div className='list-title'>
         {video.snippet.title}
       </div>
-    </span>
+    </div>
   )
 }
 export default VideoListCard
